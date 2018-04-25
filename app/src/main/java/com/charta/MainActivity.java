65b3e6f9576fab32297019e628b01f87cf4a1621
victorcaldas <contatos.caldas@gmail.com>
2018-04-25 13:57:07 -0300

    fix error lib android support

--- app/src/main/java/com/charta/MainActivity.java
+++ app/src/main/java/com/charta/MainActivity.java
@@ -3,11 +3,14 @@ package com.charta;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.digitalcharta.Charta;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        Charta meuMapa = new Charta(R.layout.activity_main,R.id.map);
+        meuMapa.getMap();
     }
 }
